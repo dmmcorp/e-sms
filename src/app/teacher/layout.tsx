3f91354e000e27e3f77cdc3cd5b22a51489e119e
MarkR <99254035+ReformaMark@users.noreply.github.com>
2025-04-14 02:07:26 +0800

    build: Downgrade from next15 to next14
    use the dependencies of the erms

--- src/app/teacher/layout.tsx
+++ src/app/teacher/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "@/lib/globals.css";
 import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
 import { ConvexClientProvider } from "@/components/convex-client-provider";
@@ -7,15 +6,6 @@ import MainNav from "@/components/main-nav";
 import { TeacherGuard } from "@/components/guards/teacher-guard";
 import { Toaster } from "@/components/ui/sonner";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "ERMS-Teacher",
@@ -32,7 +22,7 @@ export default function RootLayout({
       <ConvexClientProvider>
         <html lang="en">
           <body
-            className={`${geistSans.variable} ${geistMono.variable} flex flex-col antialiased min-w-screen min-h-screen`}
+            className={`flex flex-col antialiased min-w-screen min-h-screen`}
           >
             <TeacherGuard>
 
